912fe8568e465041f2afdfac1393ce820e2da40b
Max Hizon <maximohizon@gmail.com>
2024-05-06 14:24:05 +0800

    changes testing for static regeneration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -7,4 +8,12 @@ export default defineConfig({
     service: squooshImageService(),
   },
   integrations: [tailwind()],
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+    isr: true,
+  }),
 });
